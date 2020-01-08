9da5fc7e74ee833610fec99f03f453beae288e75
besh27 <beshdevelopment@gmail.com>
2020-01-08 14:30:32 -0600

    feat(theme-dev): load MDX into GraphQL

--- packages/gatsby-themes-docs/gatsby-node.js
+++ packages/gatsby-themes-docs/gatsby-node.js
@@ -7,7 +7,7 @@ exports.onPreBootstrap = ({ store }, options) => {
     const { program } = store.getState();
     const { contentPath } = withDefaults(options);
     const dir = path.join(program.directory, contentPath);
-    if(!fs.existsSync(dir)){
+    if (!fs.existsSync(dir)) {
         mkdirp.sync(dir)
     }
 
