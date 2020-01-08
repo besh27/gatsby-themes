8d58a3ac8a05a78d94329601061334c33d32e143
besh27 <beshdevelopment@gmail.com>
2020-01-08 15:16:56 -0600

    feat(theme-dev): add schema customization

--- packages/gatsby-themes-docs/gatsby-node.js
+++ packages/gatsby-themes-docs/gatsby-node.js
@@ -11,4 +11,72 @@ exports.onPreBootstrap = ({ store }, options) => {
         mkdirp.sync(dir)
     }
 
+};
+
+exports.createSchemaCustomization = ({ actions }) => {
+    actions.createTypes(`
+        type DocsPage implements Node @dontInfer {
+            id: ID!
+            title: String!
+            path: String!
+            updated: Date! @dateformat
+            body: String!
+        }
+    `)
 }
+
+exports.onCreateNode = ({ node, actions, getNode, createNodeId }, options) => {
+    const { basePath } = withDefaults(options);
+    const parent = getNode(node.parent);
+
+    // Only work on MDX files that were loaded by this theme
+    if (
+        node.internal.type !== 'Mdx' ||
+        parent.sourceInstanceName !== 'gatsby-theme-docs'
+    ) {
+        return;
+    }
+
+    // Treat .index.mdx link 'index.html (i.e. docs/ vs docs/index)
+    const pageName = parent.name !== 'index' ? parent.name : '';
+
+    actions.createNode({
+        id: createNodeId(`DocsPage-${node.id}`),
+        title: node.frontmatter.title || parent.name,
+        updated: parent.modifiedTime,
+        path: path.join('/', basePath, parent.relativeDirectory, pageName),
+        parent: node.id,
+        internal: {
+            type: 'DocsPage',
+            contentDigest: node.internal.contentDigest,
+        }
+    })
+}
+
+exports.createResolvers = ({ createResolvers }) => {
+    createResolvers({
+        DocsPage: {
+            body: {
+                type: 'String!', // needs to match the declaration from above.
+                resolve: (source, args, context, info) => {
+                    /*
+                        source: the actual data or node
+                        args: data format, filters, etc
+                        context: information about the node
+                        info: information about the schema. 
+                    */
+                    const type = info.schema.getType('Mdx');
+                    const mdxFields = type.getFields();
+                    const resolver = mdxFields.body.resolve;
+                    const mdxNode = context.nodeModel.getNodeById({ id: source.parent});
+
+                    return resolver(mdxNode, args, context, {
+                        fieldname: 'body',
+                    });
+                },
+            }
+        }
+    })
+}
+
+// after editting node, run 'yarn workspace theme-dev gatsby clean' to remove the public and cache dirs and run a fresh start. 
